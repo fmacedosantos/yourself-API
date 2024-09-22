d3b793e67f09bc6735f276574efabbf2e743adee
fmacedosantos <felipemacedo986@gmail.com>
2024-09-22 18:52:13 -0300

    Autenticando o usuário

--- usuario/routes.js
+++ usuario/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { UsuarioController } from "./controller.js";
+import { authenticateToken } from '../middlewares/authenticate-jwt.js';
 
 const app = express();
 const usuarioController = new UsuarioController();
@@ -7,5 +8,9 @@ const usuarioController = new UsuarioController();
 app.post('/cadastrar', (req, res) => {
     usuarioController.cadastrarUsuario(req, res);
 });
+
+app.post('/login', authenticateToken, (req, res) => {
+    res.status(200).send({ message: 'Usuário autenticado com sucesso!', usuario: req.usuario });
+})
 
 export const usuarioRouter = app;
