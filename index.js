5c0e4e42d7b355730f808aba12787827f11fec9d
fmacedosantos <felipemacedo986@gmail.com>
2024-09-22 15:13:46 -0300

    Iniciando o projeto com express

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import admin from 'firebase-admin';
+import bodyParser from 'body-parser';
+import { usuarioRouter } from './usuario/routes.js';
+
+const app = express();
+
+// inicializando o acesso ao banco de dados e credenciando a chave de conta de servico
+admin.initializeApp({
+    credential: admin.credential.cert("serviceAccountKey.json")
+});
+
+// middleware pra interpretar o corpo da requisição como json
+app.use(bodyParser.json());
+
+// rotas de usuario
+app.use('/usuario', usuarioRouter);
+
+app.listen(3000, () => console.log('API REST iniciada em http://localhost:3000'));
