408e0abf4cb28b7b7ff880c8739892d55590c9ff
fmacedosantos <felipemacedo986@gmail.com>
2024-09-26 11:10:20 -0300

    Usando middlewares de validação

--- atividade/routes.js
+++ atividade/routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { AtividadeController } from "./controller.js";
+import { validarCadastroAtividade } from '../middlewares/validation.js';
 
 const app = express();
-const atividadeController = new AtividadeController()
+const atividadeController = new AtividadeController();
 
-app.post('/cadastrar', (req, res) => {
-    atividadeController.registrarAtividade(req, res)
-})
+app.post('/cadastrar', validarCadastroAtividade, (req, res) => {
+    atividadeController.registrarAtividade(req, res);
+});
 
-export const atividadeRouter = app
+export const atividadeRouter = app;
---
+++ middlewares/validation.js
@@ -0,0 +1,15 @@
+export const validarCadastroUsuario = (req, res, next) => {
+    const { email, nome, nomeUsuario, senha } = req.body;
+    if (!email || !nome || !nomeUsuario || !senha) {
+        return res.status(400).json({ message: "Todos os campos são obrigatórios!" });
+    }
+    next();
+};
+
+export const validarCadastroAtividade = (req, res, next) => {
+    const { titulo, categoria, dificuldade, tempoConcentracao, emailUsuario } = req.body;
+    if (!titulo || !categoria || !dificuldade || !tempoConcentracao || !emailUsuario) {
+        return res.status(400).json({ message: "Todos os campos são obrigatórios!" });
+    }
+    next();
+};
