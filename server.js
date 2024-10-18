83d653a8c0da390d2e7487d1e8210530fad20a87
fmacedosantos <felipemacedo986@gmail.com>
2024-10-18 16:40:24 -0300

    Retirando duplicatas

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 // importações
 import express from "express";
-import admin from 'firebase-admin';
 import bodyParser from 'body-parser';
 import { usuarioRouter } from "./src/routes/user.routes.js";
 import { atividadeRouter } from "./src/routes/activity.routes.js";
 import { itemRouter } from "./src/routes/item.routes.js";
+import admin from "./firebase.js";
 
 // criando a aplicação express
 const app = express();
 
 // inicializando o acesso ao banco de dados e credenciando a chave de conta de servico
-if (!admin.apps.length) {  
-    admin.initializeApp({
-        credential: admin.credential.cert("serviceAccountKey.json")
-    });
-}
+const inicandoBanco = admin;
 
 // middleware pra interpretar o corpo da requisição como json
 app.use(bodyParser.json());
