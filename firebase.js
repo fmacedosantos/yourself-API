50f271b827bfd03bf3293522513355fee6fb7f2d
fmacedosantos <felipemacedo986@gmail.com>
2024-12-07 20:38:18 -0300

    Usando variáveis de ambiente (deploy com Heroku)

--- firebase.js
+++ firebase.js
@@ -1,8 +1,37 @@
 const admin = require('firebase-admin');
 
-if (!admin.apps.length) {  
+const requiredEnvVars = [
+    'FIREBASE_PROJECT_ID',
+    'FIREBASE_PRIVATE_KEY_ID',
+    'FIREBASE_PRIVATE_KEY',
+    'FIREBASE_CLIENT_EMAIL',
+    'FIREBASE_CLIENT_ID',
+    'FIREBASE_AUTH_URI',
+    'FIREBASE_TOKEN_URI',
+    'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
+    'FIREBASE_CLIENT_X509_CERT_URL',
+];
+
+for (const varName of requiredEnvVars) {
+    if (!process.env[varName]) {
+        throw new Error(`A variável de ambiente ${varName} não está definida.`);
+    }
+}
+
+if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert("serviceAccountKey.json")
+        credential: admin.credential.cert({
+            type: "service_account",
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+            private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CLIENT_ID,
+            auth_uri: process.env.FIREBASE_AUTH_URI,
+            token_uri: process.env.FIREBASE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+        }),
     });
 }
 
